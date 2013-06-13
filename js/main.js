765330b631af49a09b0acf2af5744635eda662e3
Alessio <alessio@plasticpanda.com>
2013-06-13 13:18:28 +0200

    added more pandas

--- js/main.js
+++ js/main.js
@@ -44,11 +44,28 @@
       }
     );
 
+    
+
     setTimeout(function () {
-      var pandagif = '<img style="display:none; position: absolute; width: 50px; top: -38px; left: 50%; margin-left: -25px;" src="http://30.media.tumblr.com/tumblr_livy7oxLC41qc9z7yo1_500.gif" id="pandagif">';
+
+      var pandaPics = [
+        'http://25.media.tumblr.com/tumblr_lnnchgjeYL1qlue6co1_100.gif',
+        'http://24.media.tumblr.com/d066094273e6b365cff0fd8a4b86ad3f/tumblr_mj0tagqZbK1qcsy8ho1_100.gif',
+        'http://24.media.tumblr.com/tumblr_m7je89YssL1rbfmzvo1_500.gif',
+        'http://25.media.tumblr.com/tumblr_lqyw2aptnY1qii6tmo1_100.gif',
+        'http://24.media.tumblr.com/tumblr_m8k7y0g5ah1rsn7yxo1_100.gif',
+        'http://24.media.tumblr.com/tumblr_ln8olsCRSW1qlue6co1_100.gif'
+      ]
+        , randomPic
+        , pandagif
+        , _r;
+
+      _r = Math.floor((Math.random() * (pandaPics.length - 1)));
+
+      randomPic = pandaPics[_r];
+      pandagif = '<img style="display:none; position: absolute; width: 50px; top: -38px; left: 50%; margin-left: -25px;" src="' + randomPic + '" id="pandagif">';
 
       $('.page-container .mCSB_scrollTools').css('overflow', 'visible').find('.mCSB_draggerContainer .mCSB_dragger .mCSB_dragger_bar').append($(pandagif));
-
       $('.page-container img#pandagif').fadeIn(500);
 
     }, 100);

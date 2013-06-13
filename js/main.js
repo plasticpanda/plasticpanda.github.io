af640d4252a3eeedb77e0ef5ae73a9d079e5599e
Alessio <alessio@plasticpanda.com>
2013-06-13 11:36:55 +0200

    remove useless snap thing

--- js/main.js
+++ js/main.js
@@ -6,27 +6,11 @@
   $(function () {
 
     // some vars
-    var snapTo = []
-      , getSnaps
-      , setHeights
-      , findClosest
-      , snapScrollbar;
+    var setHeights;
 
-
-    // populate snapTo array with right positions
-    getSnaps = function () {
-      $('.page-container > .page').each(function (i, page) {
-        snapTo[i] = $(page).position().left;
-      });
-    };
-    getSnaps();
 
     // set heights for some pagecontainer/sidebar elements
     setHeights = function () {
-      $('.page-container').css({
-        'height': ($('#page').height() - (parseInt($('.page-container').css('padding-top').replace('px', ''), 10) * 2)) + 'px'
-      });
-
       $('.pure-menu > ul').css({
         'height': ($('#sidebar').height() - $('.sidebar-container').outerHeight() - 15) + 'px'
       });
@@ -36,36 +20,10 @@
     // debouncing on resize for preventing over nine thousand times calls
     $.event.special.debouncedresize.threshold = 1000;
     $(window).on('debouncedresize', function () {
-      getSnaps();
       setHeights();
-      $('.page-container').mCustomScrollbar('update');
+      //$('.page-container').mCustomScrollbar('update');
       $('.pure-menu > ul').mCustomScrollbar('update');
     });
-
-    // find which page is closest when scrolling
-    findClosest = function (num, arr) {
-      var curr = arr[0]
-        , diff = Math.abs(num - curr);
-
-      for (var val = 0; val < arr.length; val++) {
-        var newdiff = Math.abs(num - arr[val]);
-        
-        if (newdiff < diff) {
-          diff = newdiff;
-          curr = arr[val];
-        }
-      }
-
-      return curr;
-    };
-
-    // callback for pages custom scrollbar
-    snapScrollbar = function () {
-      var posX = $('.page-container .mCSB_container').position().left
-        , closestX = findClosest(Math.abs(posX), snapTo);
-      
-      $('.page-container').mCustomScrollbar('scrollTo', closestX, { scrollInertia: 350, callbacks: false });
-    };
 
     // plugin for (main content) pages scrolling
     $('.page-container').mCustomScrollbar({
@@ -77,11 +35,6 @@
           enable: false
         },
         theme: 'dark-thin',
-        callbacks: {
-          onScroll:function(){ 
-            snapScrollbar(); 
-          }
-        },
         advanced: {
           updateOnBrowserResize: true
         }
